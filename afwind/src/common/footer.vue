b04a7c9a38c26f53fdc55a97f40c435d8399134c
zxr <zxr@qq.com>
2017-08-21 17:48:12 +0800

    111
    111

---
+++ afwind/src/common/footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="footer">
+      i am footer
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.footer
+  color deeppink
+</style>
